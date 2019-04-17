65b05c1bb3efefdcc5c4903b3d76fd7861c948c5
Gabriel Pontes <33611916+ograndoptimist@users.noreply.github.com>
2019-04-16 22:42:29 -0300

    Update primos.h

--- primos.h
+++ primos.h
@@ -1,10 +1,12 @@
 #include <stdbool.h>
+#include <math.h>
 
-bool checkout(int* vetor, int tamanho_vetor, int numero)
+bool checkout(int* vetor, int numero)
 {
 	int i;
+	int cont = (int) sqrt(numero);
 
-	for(i = 0; i < tamanho_vetor; i++)
+	for(i = 0; i < cont; i++)
 	{
 		if(numero % vetor[i] == 0)
 		{
