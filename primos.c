335d648786cf12fc943fdc4dd17026834326b239
Gabriel Pontes <33611916+ograndoptimist@users.noreply.github.com>
2019-04-17 12:01:35 -0300

    Add files via upload

---
+++ primos.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "primos.h"
+#include <time.h>
+
+#define NPRIMOS 1000
+
+int main(int argc, char* argv[])
+{
+	clock_t start, end;
+     	double cpu_time_used;
+
+	start = clock();
+	int primos[NPRIMOS];
+	int n = 2;
+	int numero = 0;
+	
+	primos[0] = 2;
+	primos[1] = 3;
+	primos[2] = 5;
+
+	for(numero = 7; n < NPRIMOS; numero += 2)
+	{
+		if(checkout(primos, numero, n)) 
+		{
+			n++;
+			primos[n] = numero;
+		}		
+	}
+
+	for(n = 0; n < NPRIMOS; n++)
+		printf("%d\n", primos[n]);
+
+	end = clock();
+     	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("Tempo utilizado: %f\n", cpu_time_used);
+	
+	return 0;
+}
---
+++ primos.h
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+#include <math.h>
+
+bool checkout(int* vetor, int numero, int n)
+{
+	int i;
+	int limite = sqrt(numero);
+	
+	for(i = 0; i < limite; i++)
+	{
+		if(numero % vetor[i] == 0)
+		{
+			return false;			
+		}
+	}
+			
+	return true;
+}
