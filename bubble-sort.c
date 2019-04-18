c31a446949ca874101cf8a0a0ea2d99c407ec541
Gabriel Pontes <33611916+ograndoptimist@users.noreply.github.com>
2019-04-17 22:04:45 -0300

    Update and rename bs.c to bubble-sort.c

---
+++ bubble-sort.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "bubble-sort.h"
+
+#define TAMANHO 1000000
+
+int main(int argc, char* argv[])
+{
+	long array[TAMANHO];
+	int i;
+	
+	geraVetor(array, TAMANHO);
+	imprimeArray(array, TAMANHO);
+	
+	printf("\n");	
+	
+	bubbleSort(array, TAMANHO);
+	imprimeArray(array, TAMANHO);
+	
+	return 0;
+}
